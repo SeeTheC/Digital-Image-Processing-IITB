2b17c7a7cab8cb0860786dc50e866aec810bc77e
Khursheed Ali <khursheed.ali1991@gmail.com>
2017-08-22 01:26:48 +0530

    assign2-1 report

--- assignment2/assignment2_Filtering/1/code/myUnsharpMasking.m
+++ assignment2/assignment2_Filtering/1/code/myUnsharpMasking.m
@@ -1,20 +1,9 @@
-function outImg = myUnsharpMasking( img )
-    [row,col]=size(img);
+function outImg = myUnsharpMasking( img,hsize,sigma,scalingFactor)
     %fprintf('row=%d',col);
-    Gaussian_filt=fspecial('gaussian',[100,100],1.6);
+    Gaussian_filt=fspecial('gaussian',hsize,sigma);
     Gaussian_blur=imfilter(img,Gaussian_filt);
-    unsharp=zeros(row,col)
     %to calculate the unsharp mask
-    for i=1:1:row
-       for j=1:1:col
-            unsharp(i,j)=img(i,j)-Gaussian_blur(i,j);
-        end
-    end
-    unsharpmask=zeros(row,col)
-    for i=1:1:row
-       for j=1:1:col
-             unsharpmask(i,j)=img(i,j)+unsharp(i,j);
-        end
-    end
+    unsharp=img-Gaussian_blur;
+    unsharpmask=img+scalingFactor.*unsharp;
     outImg=unsharpmask;
 end
