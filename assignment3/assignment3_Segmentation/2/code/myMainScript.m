3d86ce35e70fc5f3a3a50ca5ee3c05a46e8d843c
Khursheed Ali <khursheed.ali1991@gmail.com>
2017-08-30 00:23:38 +0530

    Mean-shift segmentation

--- assignment3/assignment3_Segmentation/2/code/myMainScript.m
+++ assignment3/assignment3_Segmentation/2/code/myMainScript.m
@@ -1,6 +1,40 @@
 %% MyMainScript
 
+%% Initialization
 tic;
-%% Your code here
 
+fileBaboonColor='../data/baboonColor.png';
+imgBaboonColor=imread(fileBaboonColor);
+img=im2double(imgBaboonColor);
+scale=1/4;
+
+GaussianFilter=fspecial('gaussian',[100,100],1);
+GaussianBlurImage=imfilter(img,GaussianFilter);
+GaussianBlurResizedImage= imresize(GaussianBlurImage,scale);
+toc
+%%
+
+figure('name','Original Image: baboonColor');
+imshow(img);
+title('\fontsize{10}{\color{red}Original Image: baboonColor}');
+o1 = get(gca, 'Position');
+colorbar(),set(gca, 'Position', o1);
+impixelinfo;
+
+figure('name','OGaussianBlurResizedImage: baboonColor');
+imshow(GaussianBlurResizedImage);
+title('\fontsize{10}{\color{red}GaussianBlurResizedImage: baboonColor}');
+o1 = get(gca, 'Position');
+colorbar(),set(gca, 'Position', o1);
+impixelinfo;
+%%
+tic 
+
+out=myMeanShiftSegmentation(GaussianBlurResizedImage,15,0.03);
+figure('name','myMeanShiftSegmentation: baboonColor');
+imshow(out);
+title('\fontsize{10}{\color{magenta}myMeanShiftSegmentation: baboonColor}');
+o1 = get(gca, 'Position');
+colorbar(),set(gca, 'Position', o1); 
+impixelinfo;
 toc;
