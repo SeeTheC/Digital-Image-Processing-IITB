754177ceb9ae4197965340f63baa1727341d9388
Khursheed Ali <khursheed.ali1991@gmail.com>
2017-10-23 15:12:54 +0530

    mean sqr error. Problem 6

--- assignment5/assignment5_DFT/6/code/myPCADenoising2.m
+++ assignment5/assignment5_DFT/6/code/myPCADenoising2.m
@@ -1,107 +1,81 @@
-function [img2]=myPCADenoising2(img1,windowSize,patchSize,L,sigma)
+function [outImg]=myPCADenoising3(img,windowSize,patchSize,L,sigma)
     % padding zeros outside the image. For simple masking
-    [row,col]=size(img1);
-    pad=floor((windowSize+patchSize)/2);
-    paddedCorruptedImage= [ zeros(row,pad) img1 zeros(row,pad)];
-    paddedCorruptedImage= [ zeros(pad,col+(2*pad));paddedCorruptedImage;zeros(pad,col+(2*pad))];
-    img2=zeros((row+patchSize),(col+patchSize));
-    
-    for i=1:1:row
-        for j=1:1:col
-            %fprintf('%d',i);
-            filtValMat=PCAFilteredValue(paddedCorruptedImage,[(i+pad) (j+pad)],windowSize,patchSize,L,sigma);
-            [xp1,yp1,xp2,yp2]=getPatchCoordinate(patchSize,[floor(i+patchSize/2) floor(j+patchSize/2)]);
-            img2(xp1:xp2,yp1:yp2)=img2(xp1:xp2,yp1:yp2)+filtValMat;
+    [row,col]=size(img);
+    outImg=zeros(row,col); 
+    pad=floor((windowSize-1)/2);
+    imgPad=padarray(img,[pad,pad],0,'both');
+    patchAddedCount=zeros(row,col);
+    for i=1:1:row-patchSize+1
+        for j=1:1:col-patchSize+1
+            xi=i+pad;yi=j+pad;            
+            filtValMat=PCAFilteredValue(imgPad,[xi,yi],windowSize,patchSize,L,sigma);
+            
+            [xp1,yp1,xp2,yp2]=getPatchCoordinate([i,j],patchSize);            
+            outImg(xp1:xp2,yp1:yp2)=outImg(xp1:xp2,yp1:yp2)+filtValMat;
+            patchAddedCount(xp1:xp2,yp1:yp2)=patchAddedCount(xp1:xp2,yp1:yp2)+1;
         end
     end
-    
-    img2=img2./(patchSize*patchSize);%wrong at border
-    img2=img2(patchSize/2:end-(patchSize/2+1),patchSize/2:end-(patchSize/2+1));
+    outImg=outImg./patchAddedCount;  
 end
-function [filteredVal]=PCAFilteredValue(img,point,windowSize,patchSize,L,sigma)
-    [x1,y1,x2,y2]=getWindowCoordinate(windowSize,patchSize,point);
-    imgWindow=img(x1:x2,y1:y2);
-    % get ref patch
-    [xp1,yp1,xp2,yp2]=getPatchCoordinate(patchSize,point);
-    imgPatch=img(xp1:xp2,yp1:yp2);
-    % creating P matrix of size (patchSize^2)*L
-    lLeastVal=zeros(L,3);
-    meanSquareValues=zeros(windowSize,windowSize);
-    for i=1:1:windowSize
-        for j=1:1:windowSize
-            sx=(i+floor((patchSize+1)/2));sy=(j+floor((patchSize+1)/2));            
-            meanSquareValues(i,j)=meanValues(imgWindow,imgPatch,[sx sy],patchSize);
-            if L>=((i-1)*windowSize+j)
-                lLeastVal(((i-1)*windowSize+j),1)=meanSquareValues(i,j);
-                lLeastVal(((i-1)*windowSize+j),2)=sx;
-                lLeastVal(((i-1)*windowSize+j),3)=sy;
-            else
-                if (L==((i-1)*windowSize+j+1))
-                    lLeastVal=sortrows(lLeastVal,1);
-                end
-                for p=1:1:L
-                    if meanSquareValues(i,j)<lLeastVal(p,1)
-                        lLeastVal(L,1)=meanSquareValues(i,j);
-                        lLeastVal(L,2)=sx;
-                        lLeastVal(L,3)=sy;
-                        lLeastVal=sortrows(lLeastVal,1);
-                        break
-                    end
-                end
-            end
-        end
-    end
+
+function [filteredVal]=PCAFilteredValue(img,point,windowSize,patchSize,L,sigma)        
+    [xw1,yw1,xw2,yw2]=getWindowCoordinate(point,windowSize);
+    [xp1,yp1,xp2,yp2]=getPatchCoordinate(point,patchSize);    
+    croppedImg=img(xw1:xw2,yw1:yw2);        
+    refPatch=reshape(img(xp1:xp2,yp1:yp2),patchSize^2,1);
     
-    rowP=patchSize^2;colP=L;
-    P=zeros(rowP,colP);
-    for l=1:1:L
-        [x1,y1,x2,y2]=getPatchCoordinate(patchSize,[lLeastVal(l,2) lLeastVal(l,3)]);
-        patchs=imgWindow(x1:x2,y1:y2);
-        vector = reshape(patchs,patchSize*patchSize,1);
-        P(:,l)=vector;
-    end
+    [patchImg,dissimilarity]=convertImgToPatch(croppedImg,refPatch,patchSize);   
+    [sorted,order]=sort(dissimilarity);
+    similarPatch=patchImg(:,order(1:L));
+     
     % to compute eigen vectors and eigen-coeff of P
-    [V D] = eig(P*P'); 
-    VT=V';
-    alphaEigenCoff=VT*P;
-    % Wiener Filter
-    alphaijSq=alphaEigenCoff.^2;
-    alphaijSqSum=sum(alphaijSq,2)/L;
-    alphaj=alphaijSqSum-sigma^2;
-    alphaj=(alphaj>0).*alphaj;
-    k=1./(1.+((sigma^2)./alphaj));
-    alphaEigenCoffDenoised=k.*alphaEigenCoff(:,1);
-    filteredVal=reshape((V*alphaEigenCoffDenoised),patchSize,patchSize);
+    p=similarPatch;
+    ppt=p*p';
+    [v d]=eig(ppt);
+    eignCoffMat=v'*p;
+    eignCoffRefPatch=v'*refPatch;
+   
+    alphaBar=max(0,(sum(eignCoffMat.^2,2)./L)-(sigma^2));
+    kFactor=1./(1+ (sigma^2./alphaBar));
+    alphaEigenCoffDenoised=kFactor.*eignCoffRefPatch;   
+    qDenoised=(v*alphaEigenCoffDenoised);
+    filteredVal=reshape(qDenoised,patchSize,patchSize);
+    
 end
 
-function [meanVal]=meanValues(img,patchq,point,patchSize)
-    [x1,y1,x2,y2]=getPatchCoordinate(patchSize,point);
-    patchs=img(x1:x2,y1:y2);
-    intensityDiff=(patchq-patchs);
-    intensityDiffSq=intensityDiff.^2;
-    meanVal=sum(sum(intensityDiffSq));
+% Returns the  patchImg of dimension (patchSize^2)x (No. of patchs)
+function [patchImg,dissimilarity]=convertImgToPatch(img,refVector,patchSize)
+    [row,col]=size(img);
+    % Init
+    noOfPatch=(row-patchSize)*(col-patchSize);
+    vectorSize=patchSize^2;
+    patchImg=zeros(vectorSize,noOfPatch);
+    dissimilarity=zeros(1,noOfPatch);
+    pk=1;
+    % Fetching patch from image and trasforming it into vector 
+    for i=1:row-patchSize+1;
+        for j=1:col-patchSize+1;
+            [x1,y1,x2,y2]=getPatchCoordinate([i,j],patchSize);
+            vector=reshape(img(x1:x2,y1:y2),vectorSize,1);
+            patchImg(:,pk)=vector;
+            dissimilarity(pk)=sum((vector-refVector).^2);
+            pk=pk+1;
+        end
+    end        
 end
 
-% Return the [x1 y1 x2 y2] coordinate of window with center as centerCoordinate
-function [x1,y1,x2,y2]=getWindowCoordinate(windowSize,patchSize,centerCoordinate)
-    %m=imageDim(1);n=imageDim(2);
-    x=centerCoordinate(1);y=centerCoordinate(2);    
-    N=floor((windowSize+patchSize)/2);
-    x1=x-N;
-    x2=x+N;
-    y1=y-N;
-    y2=y+N;
- 
+
+% Getting Window Coordinate assuming point as center
+function [x1,y1,x2,y2]=getWindowCoordinate(centerPoint,windowSize)
+    x=centerPoint(1);y=centerPoint(2);
+    halfSize=floor((windowSize-1)/2);
+    x1=x-halfSize;y1=y-halfSize;    
+    x2=x+halfSize;y2=y+halfSize; 
 end
 
-
-function [x1,y1,x2,y2]=getPatchCoordinate(patchSize,centerCoordinate)
-    %m=imageDim(1);n=imageDim(2);
-    x=centerCoordinate(1);y=centerCoordinate(2);    
-    N=floor((patchSize-1)/2);   
-    x1=x-N;
-    x2=x+N;
-    y1=y-N;
-    y2=y+N;
- 
+% Getting Patch Coordinate
+function [x1,y1,x2,y2]=getPatchCoordinate(topLeftPoint,patchSize)
+    x=topLeftPoint(1);y=topLeftPoint(2);           
+    x1=x;y1=y;    
+    x2=x+patchSize-1;y2=y+patchSize-1; 
 end
